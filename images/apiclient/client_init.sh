788e96f0508d92caa8279c1a0eb2f64dbcd5fd0f
pdonorio <tempo.pause@gmail.com>
2016-09-08 15:59:35 +0200

    Skipping client request if server is down

--- images/apiclient/client_init.sh
+++ images/apiclient/client_init.sh
@@ -6,15 +6,19 @@ echo "Login"
 export CREDENTIALS="username=user@nomail.org password=test"
 TOKEN=`http POST $SERVER/auth/login $CREDENTIALS | jq '.Response.data.token' | tr -d '"'`
 
-#echo $TOKEN
-echo "Logged"
-export AUTH="Authorization:Bearer $TOKEN"
+if [ "$TOKEN" == "" ]; then
+        echo ""
+        echo "Failed to connect to Flask server..."
+else
+        echo "Logged"
+        export AUTH="Authorization:Bearer $TOKEN"
 
-echo "You can now query the API SERVER. Examples:"
-echo ""
-echo "- Query the status endpoint and parse the JSON response"
-echo '$ http GET $SERVER/api/status | jq "."'
-echo ""
-echo "- Query a protected endpoint by passing the saved token"
-echo '$ http GET $SERVER/auth/profile "$AUTH"'
+        echo "You can now query the API SERVER. Examples:"
+        echo ""
+        echo "- Query the status endpoint and parse the JSON response"
+        echo '$ http GET $SERVER/api/status | jq "."'
+        echo ""
+        echo "- Query a protected endpoint by passing the saved token"
+        echo '$ http GET $SERVER/auth/profile "$AUTH"'
+fi
 echo ""
